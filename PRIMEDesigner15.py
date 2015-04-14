6538a7ed9f5dcbf3e016e08ad4ac9ae9aa772b1f
marict <marictdude@yahoo.com>
2015-04-14 14:49:55 -0700

    Added the PRIMEDesigner15.py file
    Is the python logic file, does not contain graphical information.

---
+++ PRIMEDesigner15.py
@@ -0,0 +1,10 @@
+class Game
+	"""A game that represents the 9 rooms, all the doors, and various
+     particular form of graphic art."""
+  """It is important for the myjson.py module that the constructor
+     have arguments for each instance component, because it will
+     use this __init__ method to reconstruct arbitrary instances
+     for the class from json representations in a database.
+     Except for the self argument and any keyword arguments,
+     there must be a 1-to-1 correspondence between parameters
+     in the __init__ method and the actual instance items."""
