ce668400ef8e3d2f473b198a5ed0451956670700
marict <marictdude@yahoo.com>
2015-05-22 14:24:34 -0700

    Moved stuff around.

--- CamanComms.js
+++ CamanComms.js
@@ -1,42 +1,3 @@
-window.onload = function() {
-	Caman.Pixel.prototype.coordinatesToLocation = Caman.Pixel.coordinatesToLocation
-    Caman.Pixel.prototype.locationToCoordinates = Caman.Pixel.locationToCoordinates
-	Caman.Pixel.prototype.putPixelRelative = function(horiz, vert, rgba) {
-      var newLoc;   // this variable is now consistently named, not newLoc and nowLoc
-
-      if (this.c == null) {
-        throw "Requires a CamanJS context";
-      }
-      newLoc = this.loc + (this.c.dimensions.width * 4 * (vert * -1)) + (4 * horiz);
-      if (newLoc > this.c.pixelData.length || newLoc < 0) {
-        return;
-      }
-      this.c.pixelData[newLoc] = rgba.r;
-      this.c.pixelData[newLoc + 1] = rgba.g;
-      this.c.pixelData[newLoc + 2] = rgba.b;
-      this.c.pixelData[newLoc + 3] = rgba.a;
-      return true;
-    };
-	Caman.Filter.register("getPixels", function (test) {
-		this.process("getPixels", function (rgba) {
-			Y = rgba.locationXY().y; // e.g. {x: 0, y: 0}
-			alert("r = " + rgba.r);
-			globalImg[Y].push(new PixelWrapper(rgba.r,rgba.g,rgba.b,rgba.a));
-		});
-		return this;
-	});
-	Caman.Filter.register("putPixels", function (width) {
-		this.process("putPixels", function (rgba) {
-			X = rgba.locationXY().x; // e.g. {x: 0, y: 0}
-			Y = rgba.locationXY().y;
-			//console.log(globalImg[X,Y])
-			rgba.putPixel(X, Y, {r : globalImg[X,Y].r, g : globalImg[X,Y].g,
-						b : globalImg[X,Y].b, a : globalImg[X,Y].a});
-		});
-		return this;
-	});
-	console.log("HELLO from CamanComms!")
-}
 
 var globalImg = [];
 var globalCounter = 0;
@@ -70,34 +31,75 @@ function CamanComms(canvasId, imagePath) {
 		Caman(this.canvasId, this.imagePath, function () {
 			var w = this.imageWidth()
 			var h = this.imageHeight()
-			alert("the width and height happened")
+			alert("width = " + this.imageWidth())
 			for(var x = 1; x < w + 1; x++) {
 				globalImg[x] = [];
 			}
-			alert("globalImg has things!")
+			alert("globalImg has space!")
 			console.log(globalImg)
-			this.getPixels("test").render();
+			globalImg[1].push(new PixelWrapper(100,101,102,103));
+			console.log(globalImg)
+			/*this.getPixels("test");
 			console.log(globalImg)
 			alert("globalImg is full of pixels!")
+			
 			var flippedImg = []
 			for(var x = 1; x < w + 1; x++) {
 				flippedImg[x] = [];
 				for(var y = 1; y < h + 1; y++) {
-					/*console.log("x: " + x + "y: " + y)
-					console.log("flippedImg: ")
-					console.log(w + 1 - x)
-					console.log(h + 1 - y)
-					console.log(globalImg[w + 1 - x][h + 1 - y])
-					console.log(globalImg[10][16])*/
 					flippedImg[x][y] = globalImg[w + 1 - x][h + 1 - y]
 				}
 			}
+			
 			alert("we made a flipped img!")
 			globalImg = flippedImg
 			alert("we gave the flipped img to the glbal one")
 			this.putPixels().render();
 			alert("we put them into the caman pixels")
-			this.render();
+			this.render();*/
 		});
 	};
+};
+
+window.onload = function() {
+	
+	Caman.Pixel.prototype.coordinatesToLocation = Caman.Pixel.coordinatesToLocation
+    Caman.Pixel.prototype.locationToCoordinates = Caman.Pixel.locationToCoordinates
+	Caman.Pixel.prototype.putPixelRelative = function(horiz, vert, rgba) {
+	  var newLoc;   // this variable is now consistently named, not newLoc and nowLoc
+
+      if (this.c == null) {
+        throw "Requires a CamanJS context";
+      }
+      newLoc = this.loc + (this.c.dimensions.width * 4 * (vert * -1)) + (4 * horiz);
+      if (newLoc > this.c.pixelData.length || newLoc < 0) {
+        return;
+      }
+      this.c.pixelData[newLoc] = rgba.r;
+      this.c.pixelData[newLoc + 1] = rgba.g;
+      this.c.pixelData[newLoc + 2] = rgba.b;
+      this.c.pixelData[newLoc + 3] = rgba.a;
+      return true;
+    };
+	
+	Caman.Filter.register("getPixels", function (test) {
+		this.process("getPixels", function (rgba) {
+			Y = rgba.locationXY().y; // e.g. {x: 0, y: 0}
+			alert("r = " + rgba.r);
+			globalImg[Y].push(new PixelWrapper(rgba.r,rgba.g,rgba.b,rgba.a));
+		});
+		return this;
+	});
+	
+	Caman.Filter.register("putPixels", function () {
+		this.process("putPixels", function (rgba) {
+			X = rgba.locationXY().x; // e.g. {x: 0, y: 0}
+			Y = rgba.locationXY().y;
+			//console.log(globalImg[X,Y])
+			rgba.putPixel(X, Y, {r : globalImg[X,Y].r, g : globalImg[X,Y].g,
+						b : globalImg[X,Y].b, a : globalImg[X,Y].a});
+		});
+		return this;
+	});
+	console.log("HELLO from CamanComms!")
 };
