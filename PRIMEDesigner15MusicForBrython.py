9aedf637d0e6b2ca117bdbdb5577e8e70c51049a
marict <marictdude@yahoo.com>
2015-06-23 00:01:52 -0700

    Added infrastructures for receiving text files

--- PRIMEDesigner15MusicForBrython.py
+++ PRIMEDesigner15MusicForBrython.py
@@ -2,5 +2,40 @@
 
 	Handle PRIME music stuff
 '''
+from browser import document, window, alert, console, html
+from javascript import JSObject, JSConstructor
 
+from browser import document, window, alert, console, ajax
+import time
+from javascript import JSObject, JSConstructor
+
+# Loads in a text file containing notes and seconds in between.
+def recieveFile(req):
+	alert("hello?")
+	if req.status == 200 or req.status == 0:
+		console.log(req.text)
+	else:
+		console.log("error" + req.text)
+
+def err_msg():
+	alert("file couldn't be opened after %s seconds" %timeout)
+
+timeout = 4 
+
+# Creates a series of wad.js oscillators out of the contents of the text file.
+def createSong(url):
+	
+	req = ajax.ajax()
+	req.bind("complete",recieveFile)
+	req.set_timeout(timeout,err_msg)
+	req.open('GET',url,True)
+	req.send()
+
+
+string = 'testMusic.txt?foo=%s'
+loadMusicFile('testMusic.txt')
+
+
+		
+	
 
--- Test_files/musicTest.py
+++ Test_files/musicTest.py
@@ -1,141 +1,36 @@
-from browser import window, document, html, alert, svg, console
-from javascript import JSConstructor
+'''PRIMEDesigner15MusicForBrythonTest
 
-pitchesArray = [ # Just an array of note names. This can be useful for mapping MIDI data to notes.
-        'C0',
-        'C#0',
-        'D0',
-        'D#0',
-        'E0',
-        'F0',
-        'F#0',
-        'G0',
-        'G#0',
-        'A0',
-        'A#0',
-        'B0',
-        'C1',
-        'C#1',
-        'D1',
-        'D#1',
-        'E1',
-        'F1',
-        'F#1',
-        'G1',
-        'G#1',
-        'A1',
-        'A#1',
-        'B1',
-        'C2',
-        'C#2',
-        'D2',
-        'D#2',
-        'E2',
-        'F2',
-        'F#2',
-        'G2',
-        'G#2',
-        'A2',
-        'A#2',
-        'B2',
-        'C3',
-        'C#3',
-        'D3',
-        'D#3',
-        'E3',
-        'F3',
-        'F#3',
-        'G3',
-        'G#3',
-        'A3',
-        'A#3',
-        'B3',
-        'C4',
-        'C#4',
-        'D4',
-        'D#4',
-        'E4',
-        'F4',
-        'F#4',
-        'G4',
-        'G#4',
-        'A4',
-        'A#4',
-        'B4',
-        'C5',
-        'C#5',
-        'D5',
-        'D#5',
-        'E5',
-        'F5',
-        'F#5',
-        'G5',
-        'G#5',
-        'A5',
-        'A#5',
-        'B5',
-        'C6',
-        'C#6',
-        'D6',
-        'D#6',
-        'E6',
-        'F6',
-        'F#6',
-        'G6',
-        'G#6',
-        'A6',
-        'A#6',
-        'B6',
-        'C7',
-        'C#7',
-        'D7',
-        'D#7',
-        'E7',
-        'F7',
-        'F#7',
-        'G7',
-        'G#7',
-        'A7',
-        'A#7',
-        'B7',
-        'C8'
-    ];
+	Handle PRIME music stuff
+'''
+from browser import document, window, alert, console, ajax
+import time
+from javascript import JSObject, JSConstructor
 
-pitchNo = 0
-pitch = pitchesArray[pitchNo]
-def increasePitch():
-	global pitchNo
-	global pitch
-	pitch = pitchesArray[pitchNo]
-	pitchNo = pitchNo + 1
-	alert(pitch)
+# Creates a series of wad.js oscillators out of the contents of the text file.
+def createSong(req):
+	alert("hello?")
+	if req.status == 200 or req.status == 0:
+		console.log(req.text)
+	else:
+		console.log("error" + req.text)
 
-def playAudio():
-	global pitch
-	alert("pitch in playAudio = " + pitch)
-	audio = wadConstructor({
-		'source': 'testSong2.mp3',
-		'loop': False,
-		'env' : {
-			'hold' : 5
-		}
-	})
+def err_msg():
+	alert("server didn't reply after %s seconds" %timeout)
+
+timeout = 4 
+
+# Loads in a text file containing notes and seconds in between.
+def loadMusicFile(url):
 	
-	audio.play()
+	req = ajax.ajax()
+	req.bind("complete",createSong)
+	req.set_timeout(timeout,err_msg)
+	req.open('GET',url,True)
+	req.send()
 
 
-	
-wadConstructor = JSConstructor(window.Wad)
-
-button = html.BUTTON('Play Audio')
-button.onclick = playAudio
-
-button2 = html.BUTTON('Increase Pitch')
-button2.onclick = increasePitch
-
-document <= button
-document <= button2
-
+string = 'testMusic.txt?foo=%s'
+loadMusicFile('testMusic.txt')
 
 
 
